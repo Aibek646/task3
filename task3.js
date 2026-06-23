546b5eeea65ce831520dcdc58c5243d49ccea6a9
Aibek Ramazanov <r4mazanov.aibek@yandex.ru>
2026-06-23 17:48:02 +0500

    pushed code

--- task3.js
+++ task3.js
@@ -1,26 +1,30 @@
 const http = require('http');
-function gcd(a,b) {
-    while(b) {
+
+function gcd(a, b) {
+    while (b) {
         let t = b;
         b = a % b;
         a = t;
     }
     return a;
 }
-function lcm(a,b) {
-    return (a * b) / gcd(a,b);
+
+function lcm(a, b) {
+    return (a * b) / gcd(a, b);
 }
-const server = http.createServer((req,res) => {
+
+const server = http.createServer((req, res) => {
     const params = new URL(req.url, 'http://localhost').searchParams;
     const x = parseInt(params.get('x'));
     const y = parseInt(params.get('y'));
-    if(!Number.isInteger(x) || !Number.isInteger(y) || x < 1 || y < 1) {
+
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 1 || y < 1) {
         res.end('NaN');
     } else {
-        res.end(String(lcm(x,y)))
+        res.end(String(lcm(x, y)));
     }
-})
+});
 
 server.listen(3000, () => {
-    console.log(`Server started on port 3000!`);
-})
+    console.log('Server started on port 3000!');
+});
